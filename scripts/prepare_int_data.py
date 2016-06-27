2bb53341d4f30febbbbae95d601a9b0efb21db14
Ke Li <keli1@t01.clsp.jhu.edu>
2016-06-26 23:42:18 -0400

    modified scripts/prepare_int_data.py after /internal/pocolm_common.py is ready

--- scripts/prepare_int_data.py
+++ scripts/prepare_int_data.py
@@ -4,6 +4,12 @@
 from __future__ import print_function
 import re, os, argparse, sys, math, warnings, subprocess, shutil, threading
 
+# make sure scripts/internal is on the pythonpath.
+sys.path = [ os.path.abspath(os.path.dirname(sys.argv[0])) + "/internal" ] + sys.path
+
+# for ExitProgram and RunCommand
+from pocolm_common import *
+
 parser = argparse.ArgumentParser(description="This program uses the vocabulary" 
                                  "file to turn the data into ASCII-integer form," 
                                  "and to give it a standard format in preparation"
@@ -26,21 +32,73 @@ parser.add_argument("int_dir",
 
 args = parser.parse_args()
 
-# Add the script dir and the src dir to the path.
+def GetNumTrainSets(int_dir):
+  f = open(int_dir)
+  line = f.readline()
+  try:
+    a = line.split()
+    assert len(a) == 2
+    ans = int(a[0])
+  except:
+    ExitProgram("failed to get the num_train_sets from {0}".format(int_dir))
+
+  return ans 
+
+def GetNumWords(vocab):
+  command = "tail -n 1 {0}".format(vocab)
+  line = subprocess.check_output(command, shell = True)
+  try:
+    a = line.split()
+    assert len(a) == 2
+    ans = int(a[1])
+  except:
+    ExitProgram("failed to get the num_words from {0}".format(vocab))
+  
+  return ans 
+
+def GetNames(text_dir, int_dir):
+  command = "get_names.py {0} > {1}/names".format(
+              text_dir, int_dir) 
+  log_file = "{int_dir}/log/get_names.log".format(int_dir = int_dir)
+  RunCommand(command, log_file)
+
+def CopyFile(src, dest):
+  try:
+    shutil.copy(src, dest)
+  except: 
+    ExitProgram("prepare_int_data.py: error copying {0} to {1}".format(src, dest))
+
+# preparation of the train and dev data 
+def GetData(int, name):
+  if os.path.exists(args.text_dir + "/" + name + ".txt.gz"):
+    command = "set -o pipefail; gunzip -c {text_dir}/{name}.txt.gz | "\
+              "text_to_int.py {vocab} | gzip -c > {int_dir}/{int}.txt.gz "\
+               "2>{int_dir}/log/{int}.log".format(text_dir = args.text_dir, \
+               name = name, vocab = args.vocab, int_dir = args.int_dir, int = int)
+    log_file = "{int_dir}/log/{int}.log".format(int_dir = args.int_dir, int = int) 
+    output = GetCommandStdout(command, log_file)
+  else:
+    command = "set -o pipefail; cat {text_dir}/{name}.txt | text_to_int.py {vocab} "\
+               "| gzip -c > {int_dir}/{int}.txt.gz 2>{int_dir}/log/{int}.log".format(
+               text_dir = args.text_dir, name = name, vocab = args.vocab, \
+               int_dir = args.int_dir, int = int)
+    log_file = "{int_dir}/log/{int}.log".format(int_dir = args.int_dir, int = int) 
+    output = GetCommandStdout(command, log_file)
+
+# make sure 'scripts', 'scripts/internal', and 'src' directory are on the path
 os.environ['PATH'] = (os.environ['PATH'] + os.pathsep +
                       os.path.abspath(os.path.dirname(sys.argv[0])) + os.pathsep +
-                      os.path.abspath(os.path.dirname(sys.argv[0])) + "/../src" + os.pathsep +
-                      os.path.abspath(os.path.dirname(sys.argv[0])) + "/internal") 
+                      os.path.abspath(os.path.dirname(sys.argv[0])) + "/../src" + os.pathsep + 
+                      os.path.abspath(os.path.dirname(sys.argv[0])) + "/internal")
 
-# do some checking
 if os.system("validate_text_dir.py " + args.text_dir) != 0:
-  sys.exit("prepare_int_data.py: failed to validate text directory")
+  ExitProgram("command validate_text_dir.py {0} failed".format(args.text_dir))
 
 if os.system("validate_vocab.py " + args.vocab) != 0:
-  sys.exit("prepare_int_data.py: failed to validate vocab")
+  ExitProgram("command validate_vocab.py {0} failed".format(args.vocab))
 
 if not os.path.exists(os.path.abspath(os.path.dirname(sys.argv[0])) + "/text_to_int.py"):
-  sys.exit("prepare_int_data.py: expected text_to_int.py to be on the path")
+  ExitProgram("prepare_int_data.py: expected text_to_int.py to be on the path")
 
 # create the output data directory
 if not os.path.exists(args.int_dir + "/log"):
@@ -54,75 +112,30 @@ for f in filelist:
   except OSError:
     pass
 
-# read the variables 'num_train_sets' and 'num_words' from the corresponding
-# files in in_dir.
-for name in [ 'num_train_sets', 'num_words' ]:
-    f = open(args.int_dir + os.sep + name)
-    globals()[name] = int(f.readline())
-    f.close()
-
-def ExitProgram(message):
-  print(message, file=sys.stderr)
-  os._exit(1)
-
-def RunCommand(command):
-  # print the command for logging
-  print(command, file=sys.stderr)
-  if os.system(command) != 0:
-    ExitProgram("prepare_int_data.py: error running command: " + command)
-
-def GetCommandStdout(command):
-  # print the command for logging
-  print(command, file=sys.stderr)
-  try:
-    output = subprocess.check_output(command, shell = True)
-  except:
-    ExitProgram("prepare_int_data.py: error running command: " + command)
-  return output
-
-def GetNames(text_dir, int_dir):
-  command = "get_names.py {0} > {1}/names".format(
-              text_dir, int_dir) 
-  RunCommand(command)
-
-def CopyFile(src, dest):
-  try:
-    shutil.copy(src, dest)
-  except: 
-    sys.exit("prepare_int_data.py: error copying {0} to {1}".format(src, dest))
-
-# we can include the preparation of the dev data in the following
-# by adding "dev dev" to the contents of int_dir/names
-def PrepareData(int, name):
-  if os.path.exists(args.text_dir + "/" + name + ".txt.gz"):
-    command = "set -o pipefail; gunzip -c {text_dir}/{name}.txt.gz | \
-               text_to_int.py {vocab} | gzip -c > {int_dir}/{int}.txt.gz \
-               2>{int_dir}/log/{int}.log".format(text_dir = args.text_dir, \
-               name = name, vocab = args.vocab, int_dir = args.int_dir, int = int)
-    output = GetCommandStdout(command)
-  else:
-    command = "set -o pipefail; cat {text_dir}/{name}.txt | text_to_int.py {vocab} \
-               | gzip -c > {int_dir}/{int}.txt.gz 2>{int_dir}/log/{int}.log".format(
-               text_dir = args.text_dir, name = name, vocab = args.vocab, \
-               int_dir = args.int_dir, int = int)
-    output = GetCommandStdout(command)
-
-# return the names of train and dev datasets 
 GetNames(args.text_dir, args.int_dir)
 
 # copy vocab to the output directory 
 CopyFile(args.vocab, args.int_dir + "/words.txt")
 
+# get file 'num_train_sets' in int_dir from file 'names' in int_dir
+with open(args.int_dir + os.sep + "num_train_sets", "w") as f:
+  num_train_sets = GetNumTrainSets(args.int_dir + os.sep + "names")
+  f.write(str(num_train_sets))
+
+# get file 'num_words' in int_dir from vocab
+with open(args.int_dir + os.sep + "num_words", "w") as f:
+  num_words = GetNumWords(args.vocab)
+  f.write(str(num_words))
+
 # parallel/sequential processing
 threads = [] 
-f = open(args.int_dir + "/names", "r")
-for line in f:
+with open(args.int_dir + "/names", "r") as f:
+  line = f.readline()
   [int, name] = line.split()
-  threads.append(threading.Thread(target = PrepareData, args = [int, name]))
-f.close()
-threads.append(threading.Thread(target = PrepareData, args = ["dev", "dev"]))
+  threads.append(threading.Thread(target = GetData, args = [int, name]))
+threads.append(threading.Thread(target = GetData, args = ["dev", "dev"]))
 
-if args.parallel=="true":
+if args.parallel == "true":
   for t in threads: 
     t.start()
   for t in threads:
@@ -143,4 +156,4 @@ else:
 
 # validate the output data directory  
 if os.system("validate_int_dir.py " + args.int_dir) != 0:
-  sys.exit(1)
+  ExitProgram("command validate_int_dir.py {0} failed".format(args.int_dir))
